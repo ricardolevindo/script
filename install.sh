f4223755dbbbce793857e27c372bbe0b53aa2eb5
Ricardo Levindo Alves de Moraes <ricardo.levindo@gmail.com>
2020-11-25 11:50:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 
 #instalar captura de tela 
+sudo apt install flameshot
+
+#instalar wine
 sudo apt-get install wine
-wget http://app.prntscr.com/build/setup-lightshot.exe
-wine ./setup-lightshot.exe
+
+#wget http://app.prntscr.com/build/setup-lightshot.exe
+#wine ./setup-lightshot.exe
