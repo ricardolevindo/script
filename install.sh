2587bc6aad92700037b986ab74ff407f02031309
Ricardo Levindo Alves de Moraes <ricardo.levindo@gmail.com>
2020-11-30 00:30:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,3 +58,16 @@ sudo snap install disk-space-saver
 
 #Create custom QR Codes
 sudo snap install qr-code-generator-desktop
+
+
+#Instalação OpenVpn
+#First ensure that your apt supports the https transport:
+  apt install apt-transport-https
+#Install the OpenVPN repository key used by the OpenVPN 3 Linux packages  
+  wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub
+  apt-key add openvpn-repo-pkg-key.pub  
+#Then you need to install the proper repository. Replace $DISTRO with the release name depending on your Debian/Ubuntu distribution.
+  wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list
+  apt update
+#And finally the openvpn3 package can be installed  
+  apt install openvpn3
