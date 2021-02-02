7b305608e8f129d90939b97d6be068c91727372e
Ricardo Levindo Alves de Moraes <ricardo.levindo@gmail.com>
2021-02-02 11:29:23 -0300

    Create docker-coompose-centos7.sh

---
+++ docker-coompose-centos7.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+printf "Instalação do Docker-Compose no CentOS 7" 
+
+sudo yum install curl
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker-compose --version
