4004505728b65e809682a1e853b26bb1c09a0c58
Ricardo Levindo Alves de Moraes <ricardo.levindo@gmail.com>
2025-06-08 13:37:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+## verificar uso de disco com ncdu
+sudo apt install ncdu
+sudo ncdu /home
+
 ## grafana observabilidade
 wget -qO- https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
